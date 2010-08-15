d0e8346df83aa1b4b1aa83a5e4f5db45458cc8ca
Daniel Ribeiro <danrbr@gmail.com>
2010-08-15 02:21:42 -0300

    class variables and extensions

--- class_variables_and_module_extension.rb
+++ class_variables_and_module_extension.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby
+module ClassExtension
+  @@var = "module var"
+  def printvar
+    puts @@var
+  end
+end
+
+class A
+  @@var = 'A class variable'
+  extend ClassExtension
+end
+
+A.printvar #prints module var
